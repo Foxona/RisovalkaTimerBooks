025f8ab7f4d5750e82330fdf1ab969f63e3dc885
pawfen <sh0n.mishel34@gmail.com>
2021-01-28 13:21:47 +0300

    Ring-render

--- src/components/Risovalka.jsx
+++ src/components/Risovalka.jsx
@@ -2,14 +2,23 @@ import styles from './Book.module.css'
 import { useState } from 'react'
 
 export default function Risovalka(props) {
-    const [coord, setCoord] = useState({x: 0, y: 0})
+    const [coord, setCoord] = useState({ x: 0, y: 0 })
+    const [click, setClick] = useState(coord)
 
-    return <div className={styles.risovalkaContainer} onMouseMove={(e) => {
-        let x = e.clientX;
-        let y = e.clientY;
-        setCoord({x,y})
-    }}>
-        Координаты X: {coord.x} <br/>
-        Координаты Y: {coord.y}
+    return <div className={styles.risovalkaContainer}
+        onMouseMove={(e) => {
+            let x = e.clientX;
+            let y = e.clientY;
+            setCoord({ x, y })
+        }}
+        onClick={(e) => {
+            setClick(coord)
+            console.log(coord)
+            console.log(click)
+
+        }}>
+        <div className={styles.ring} style={{top: click.y, left: click.x}}></div>
+            Координаты X: {coord.x} <br />
+            Координаты Y: {coord.y}
     </div>
 }
